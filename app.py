4c2da2c9551f0ce54ea2d55fa6ad1941729692c3
Nemer Y Tamimi <nemertamimi1999@icloud.com>
2021-04-14 15:04:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import threading
 import time
 import cv2
 
-cap = cv2.VideoCapture('rtsp://admin:wireless-ip-22@176.67.56.49:554/Streaming/Channels/101')
+cap = cv2.VideoCapture('rtsp://admin:wireless-ip-22@176.67.56.49:83/Streaming/Channels/101')
+if (cap.isOpened() == False): 
+    print("Unable to read camera feed")
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
 counter = 0
 
